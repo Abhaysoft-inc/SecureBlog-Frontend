6842fb4af71b6bdb83ac0faf37f14edd9c1b438d
Abhay Vishwakarma <57512017+Abhaysoft-inc@users.noreply.github.com>
2023-10-03 11:02:19 +0530

    Update SignupPage.vue

--- src/components/SignupPage.vue
+++ src/components/SignupPage.vue
@@ -35,7 +35,7 @@
     methods: {
       async signup() {
         try {
-          const response = await axios.post(`http://localhost:3000/api/auth/signup`, {
+          const response = await axios.post(`https://secureblog.onrender.com/api/auth/signup`, {
             username: this.username,
             password: this.password,
             mobile:this.mobile,
