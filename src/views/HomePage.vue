c3d351a5d0d3d72c7e831142b100e71ee6dd70e7
Abhay Vishwakarma <57512017+Abhaysoft-inc@users.noreply.github.com>
2023-10-03 11:50:46 +0530

    Update HomePage.vue
    are yyar post pehle dikha rha hoo bard ne bataya h

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,51 +1,42 @@
 <template>
-    <div>
-      <MyNavbar />
-      <div class="container mt-4">
-        <h1>Latest Posts</h1>
-  
-        <!-- Iterate through the blog posts and display each one as a Bootstrap card with an image and summary -->
-        <div class="container my-4">
+  <div>
+    <MyNavbar />
+    <div class="container mt-4">
+      <h1>Latest Posts</h1>
+
+      <div class="container my-4">
         <div class="row">
           <div
-            v-for="post in posts"
+            v-for="(post, index) in posts.sort((a, b) => b.date - a.date)"
             :key="post._id"
             class="col-md-4 mb-4"
+            :class="{ 'latest-post': index === posts.length - 1 }"
           >
             <div class="card">
-              <!-- Display the post image -->
               <img src="@/assets/logo.png" class="card-img-top" alt="Post Image" />
-  
-              <!-- Display post title and make it a clickable link -->
+
               <router-link :to="'/blog/' + post._id">
                 <div class="card-body">
                   <h5 class="card-title">{{ post.title }}</h5>
-                  <!-- Display a summary of the post -->
-                  <!-- <p class="card-text">{{ post.summary }}</p> -->
-                  <!-- Optionally, display other post details like author, date, etc. -->
-                  <!-- <p class="card-text">{{ post.author }}</p> -->
-                  <!-- <p class="card-text">{{ formatDate(post.date) }}</p> -->
-                  <!-- "Read More" button -->
                   <router-link :to="'/blog/' + post._id" class="btn btn-primary">Read More</router-link>
                 </div>
               </router-link>
             </div>
           </div>
         </div>
-        </div>
       </div>
     </div>
-  </template>
-  
+  </div>
+</template>
 
 <script>
 import MyNavbar from '../components/NavbarComp.vue';
 import axios from 'axios';
-export default{
-    components:{
-        MyNavbar,
-    },
-    data() {
+export default {
+  components: {
+    MyNavbar,
+  },
+  data() {
     return {
       posts: [],
     };
@@ -63,4 +54,5 @@ export default{
   created() {
     this.loadPosts(); // Fetch blog posts when the component is created
   },
-}</script>
+};
+</script>
