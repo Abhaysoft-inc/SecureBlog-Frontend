ebc6e522b47c60ea5a8a271fdf3c5a4b86ded66e
Abhay Vishwakarma <57512017+Abhaysoft-inc@users.noreply.github.com>
2023-10-03 12:14:10 +0530

    Update main.js
    Added Bootstrap files

--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import 'bootstrap/dist/css/bootstrap.css'; // Import the router configuration
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.10.2/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
 
 const app = createApp(App);
 app.use(router);
