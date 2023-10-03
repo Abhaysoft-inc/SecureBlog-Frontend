3b28593ebffe1325b5619d9e575869c6885be69f
Abhay Vishwakarma <57512017+Abhaysoft-inc@users.noreply.github.com>
2023-10-03 14:16:58 +0530

    Update FooterComp.vue

--- src/components/FooterComp.vue
+++ src/components/FooterComp.vue
@@ -7,7 +7,7 @@
   <!-- Copyright -->
   <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
     © 2024 Copyright:
-    <a class="text-white" href="https://github.com/Abhayofc/secureblog/">SecureBlog</a>
+    <a class="text-white" href="/dashboard">SecureBlog</a>
   </div>
   <!-- Copyright -->
 </footer>
